399b07a274f4add7b137f26c5f19cbd7eeb87d8a
patbenatar <nick@gophilosophie.com>
2014-02-05 19:27:15 -0800

    Fix scope

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,6 +1,6 @@
 class Event < ActiveRecord::Base
   has_many :batches
-  has_many :ordered_batches, -> { "scraped_at DESC" }, class_name: "Batch"
+  has_many :ordered_batches, -> { order("scraped_at DESC") }, class_name: "Batch"
 
   scope :active, -> { where("start_scraping_at <= :now AND stop_scraping_at >= :now", now: Time.now) }
 end
